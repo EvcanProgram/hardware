56c8eb5636d6bf163d89a925e42feb1844a1fa31
ZwaneProgram <phuwasit.wantaya@gmail>
2024-08-20 12:09:41 +0700

    more debug

--- src/app/api/ledstate/route.js
+++ src/app/api/ledstate/route.js
@@ -1,16 +1,11 @@
-//send to rasberry 
-import { NextResponse } from 'next/server';
-
 export async function POST(request) {
     try {
         const data = await request.json();
-        
-        let picoResponse;
+        console.log('Received data:', data);
 
-        // Set the URL of the Raspberry Pi Pico server
+        let picoResponse;
         const PICO_URL = 'https://8542-27-55-71-25.ngrok-free.app';
 
-        // Send a request to the Pico based on the action
         if (data.action === 'red') {
             picoResponse = await fetch(`${PICO_URL}/red`);
         } else if (data.action === 'green') {
@@ -21,10 +16,12 @@ export async function POST(request) {
             picoResponse = await fetch(`${PICO_URL}/off`);
         }
 
+        console.log('Pico response status:', picoResponse.status);
         const picoData = await picoResponse.text();
 
         return NextResponse.json({ message: 'Action performed on Pico', data: picoData });
     } catch (error) {
+        console.error('Error processing request:', error);
         return NextResponse.json({ message: 'Error processing data', error: error.message }, { status: 500 });
     }
 }
